3835c94f4c5a4c6df5f53acbf505a9355cd1551e
Viacheslav Zhulov <slaaavyn@gmail.com>
2020-08-14 20:32:11 +0300

    implemented apiDoc

---
+++ docs/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "smart-backend",
+  "version": "0.1.0",
+  "description": "apiDoc for smart-backend",
+  "title": "apiDoc for smart-backend",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-08-14T17:25:54.016Z",
+    "url": "http://apidocjs.com",
+    "version": "0.24.0"
+  }
+});
