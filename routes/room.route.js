76b788233bd3624e0c4a22289b8af5157d2dd4bb
Viacheslav Zhulov <slaaavyn@gmail.com>
2020-08-17 16:50:57 +0300

    include documentation for the /api/room endpoints

--- routes/room.route.js
+++ routes/room.route.js
@@ -6,29 +6,154 @@ const roles = require('../utils/roles.type');
 
 const router = express.Router();
 
-/* POST create room */
+/**
+ * @api {post} /api/room?roomName=Main Create room
+ * @apiName CreateRoom
+ * @apiGroup Room
+ * @apiPermission ROLE_ADMIN
+ *
+ * @apiUse AuthorizationHeader
+ *
+ * @apiParam  {String} roomName Name for the room
+ *
+ * @apiSuccess {Object} room Room
+ * @apiSuccess {String} room._id ID of the room
+ * @apiSuccess {String} room.name name of the room
+ * @apiSuccess {Object[]} room.devices List of devices in room
+ *
+ * @apiSuccessExample Success Response:
+ *     HTTP/1.1 200 Success
+ *       {
+ *        "_id":
+ *        "name": "Main",
+ *        "devices": []
+ *       }
+ *
+ * @apiUse NotAuthorizedError
+ * @apiUse InternalServerError
+ * @apiUse ConflictError
+ **/
 router.post('/',
     [jwt.checkJwt, jwt.checkUserRole([roles.ROLE_ADMIN])],
     validators.roomNameQuery,
     roomCtrl.crateRoom);
 
-/* GET room list */
+/**
+ * @api {get} /api/room List all rooms
+ * @apiName GetRoomLst
+ * @apiGroup Room
+ * @apiPermission [ROLE_ADMIN, ROLE_USER]
+ *
+ * @apiUse AuthorizationHeader
+ *
+ * @apiSuccess {Object[]} rooms List of rooms
+ * @apiSuccess {Object} rooms.room Room
+ * @apiSuccess {String} rooms.room._id ID of the room
+ * @apiSuccess {String} rooms.name name of the room
+ * @apiSuccess {Object[]} rooms.room.devices List of devices in room
+ *
+ * @apiSuccessExample Success Response:
+ *     HTTP/1.1 200 Success
+ *     [
+ *       {
+ *        "_id":
+ *        "name": "a_room",
+ *        "devices": []
+ *       },
+ *       {
+ *        "_id":
+ *        "username": "b_room",
+ *        "devices": []
+ *       }
+ *     ]
+ *
+ * @apiUse NotAuthorizedError
+ * @apiUse InternalServerError
+ **/
 router.get('/',
-    [jwt.checkJwt, jwt.checkUserRole([roles.ROLE_ADMIN])],
+    [jwt.checkJwt, jwt.checkUserRole([roles.ROLE_ADMIN, roles.ROLE_USER])],
     roomCtrl.getRoomList);
 
-/* GET device by id */
+/**
+ * @api {get} /api/room/:roomId Get device by id
+ * @apiName GetRoom
+ * @apiGroup Room
+ * @apiPermission [ROLE_ADMIN, ROLE_USER]
+ *
+ * @apiUse AuthorizationHeader
+ *
+ * @apiParam  {String} roomId Room unique ID
+ *
+ * @apiSuccess {Object} room Room
+ * @apiSuccess {String} room._id ID of the room
+ * @apiSuccess {String} name name of the room
+ * @apiSuccess {Object[]} room.devices List of devices in room
+ *
+ * @apiSuccessExample Success Response:
+ *     HTTP/1.1 200 Success
+ *     {
+ *      "_id":
+ *      "name": "a_room",
+ *      "devices": []
+ *     }
+ *
+ * @apiUse NotAuthorizedError
+ * @apiUse InternalServerError
+ **/
 router.get('/:roomId',
-    [jwt.checkJwt, jwt.checkUserRole([roles.ROLE_ADMIN])],
+    [jwt.checkJwt, jwt.checkUserRole([roles.ROLE_ADMIN, roles.ROLE_USER])],
     roomCtrl.getRoomById);
 
-/*  PUT update room name */
+/**
+ * @api {put} /api/room/:roomId?roomName=newName Update room name
+ * @apiName UpdateRoom
+ * @apiGroup Room
+ * @apiPermission ROLE_ADMIN
+ *
+ * @apiUse AuthorizationHeader
+ *
+ * @apiParam  {String} roomId Room unique ID
+ * @apiParam  {String} roomName Name for the room
+ *
+ * @apiSuccess {Object} room Room
+ * @apiSuccess {String} room._id ID of the room
+ * @apiSuccess {String} room.name name of the room
+ * @apiSuccess {Object[]} room.devices List of devices in room
+ *
+ * @apiSuccessExample Success Response:
+ *     HTTP/1.1 200 Success
+ *       {
+ *        "_id":
+ *        "name": "newName",
+ *        "devices": []
+ *       }
+ *
+ * @apiUse NotAuthorizedError
+ * @apiUse InternalServerError
+ * @apiUse ConflictError
+ **/
 router.put('/:roomId',
     [jwt.checkJwt, jwt.checkUserRole([roles.ROLE_ADMIN])],
     validators.roomNameQuery,
     roomCtrl.updateRoom);
 
-/*  DELETE room by id */
+/**
+ * @api {delete} /api/room/:roomId Delete room by id
+ * @apiName DeleteUser
+ * @apiGroup User
+ * @apiPermission ROLE_ADMIN
+ *
+ * @apiUse AuthorizationHeader
+ *
+ * @apiParam  {String} roomId Room unique ID
+ *
+ * @apiSuccessExample Success Response:
+ *     HTTP/1.1 200 Success
+ *
+ * @apiUse NotAuthorizedError
+ * @apiUse InternalServerError
+ * @apiUse NotFoundError
+ **/
 router.delete('/:roomId',
     [jwt.checkJwt, jwt.checkUserRole([roles.ROLE_ADMIN])],
     roomCtrl.deleteRoom);
